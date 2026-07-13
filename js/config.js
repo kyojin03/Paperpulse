78c7a63521d69937a6154069e365d1bf2375d32e
Piolo Bernardino <pilbrnrdno@gmail.com>
2026-07-13 16:22:15 +0800

    Connect PaperPulse to production API

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 /* Global runtime configuration. Set apiUrl to the deployed Apps Script Web App URL before publishing. */
 window.PaperPulseConfig = Object.freeze({
-  apiUrl: 'https://script.google.com/macros/s/AKfycbwwIn5g2_gll2in1BXaL8geYX8GfbwAWVMDD9cgeFlv1JNH8yRfYk9Jl7lrrxPv_kxD/exec',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbx76jviJqixIqufneJ3YVsmIc8ke-PNogNrKqXFufN7dq10a07kwUedHuE1gkH9Kq3e/exec',
   requestTimeoutMs: 15000,
   suggestionDelayMs: 250,
   minSuggestionLength: 2,
